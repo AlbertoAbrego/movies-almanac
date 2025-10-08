cf8c364809c7fcd257edd1244969611b51e9572e
Alberto.Barcenas.Abrego <alberto.barcenas.abrego@gmail.com>
2025-10-08 04:42:18 -0600

    segundo inicio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+    return (
+        <>
+            <h1 className="font-bold underline text-blue-800"> Hola mundo </h1>
+        </>
+    )
+}
+
+export default App

d2d90cf9ac2bda31194681a6aaed26e798bcfbab
Alberto.Barcenas.Abrego <alberto.barcenas.abrego@gmail.com>
2025-10-13 13:36:57 -0600

    Algunas configuraciones base para empezar

---
+++ src/app/layout/Header.tsx
@@ -0,0 +1,7 @@
+export const Header: React.FC = () => {
+    return (
+        <div className="bg-primary-dark text-white text-sm text-center py-4 mt-6">
+            <h1 className="text-xl font-bold text-center">Movie Explorer</h1>
+        </div>
+    )
+}
